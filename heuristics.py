15ab9783cc2847886efe47d39fa9e96292bb65ec
AnankaFishbein <48636000+AnankaFishbein@users.noreply.github.com>
2025-01-31 00:33:01 +0000

    starting on heuristics

---
+++ heuristics.py
@@ -0,0 +1,7 @@
+def misplaced_tiles(state, goal, n):
+    #sum up number of 1.misplaced 2.non-empty tiles
+    return 0 #temp
+
+def manhattan_distance(state, goal, n):
+    #sum of all non-empty tile's sum of hori and verti distance to their goal tile
+    return 0 #temp
