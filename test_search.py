07cdf06c9ee46db52aed876e1eefb121daf6673c
AnankaFishbein <48636000+AnankaFishbein@users.noreply.github.com>
2025-02-09 06:39:09 +0000

    forgot this file

--- test_search.py
+++ test_search.py
@@ -65,12 +65,60 @@ if __name__ == "__main__":
         #     "n": 3,
         #     "expected_depth": None
         # },
-        {
-            "name": "case_very_hard()",
-            "initial_state": (0, 7, 2, 4, 6, 1, 3, 5, 8),
-            "n": 3,
-            "expected_depth": 24
-        }
+        # {
+        #     "name": "case_very_hard()",
+        #     "initial_state": (0, 7, 2, 4, 6, 1, 3, 5, 8),
+        #     "n": 3,
+        #     "expected_depth": 24
+        # }
+         {
+        "name": "case_depth_0",
+        "initial_state": (1, 2, 3, 4, 5, 6, 7, 8, 0),
+        "n": 3,
+        "expected_depth": 0
+    },
+    {
+        "name": "case_depth_2",
+        "initial_state": (1, 2, 3, 4, 5, 6, 0, 7, 8),
+        "n": 3,
+        "expected_depth": 2
+    },
+    {
+        "name": "case_depth_4",
+        "initial_state": (1, 2, 3, 5, 0, 6, 4, 7, 8),
+        "n": 3,
+        "expected_depth": 4
+    },
+    {
+        "name": "case_depth_8",
+        "initial_state": (1, 3, 6, 5, 0, 2, 4, 7, 8),
+        "n": 3,
+        "expected_depth": 8
+    },
+    {
+        "name": "case_depth_12",
+        "initial_state": (1, 3, 6, 5, 0, 7, 4, 8, 2),
+        "n": 3,
+        "expected_depth": 12
+    },
+    {
+        "name": "case_depth_16",
+        "initial_state": (1, 6, 7, 5, 0, 3, 4, 8, 2),
+        "n": 3,
+        "expected_depth": 16
+    },
+    {
+        "name": "case_depth_20",
+        "initial_state": (7, 1, 2, 4, 8, 5, 6, 3, 0),
+        "n": 3,
+        "expected_depth": 20
+    },
+    {
+        "name": "case_depth_24",
+        "initial_state": (0, 7, 2, 4, 6, 1, 3, 5, 8),
+        "n": 3,
+        "expected_depth": 24
+    }
     ]
 
     for case in test_cases:
